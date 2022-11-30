ff1f96ecf00f778f848677861607e53e0a758904
BenitaX <114526675+BenitaX@users.noreply.github.com>
2022-11-29 22:03:43 -0500

    Switched left and right controls into right direction

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/Mecanum_BasicOpMode_Linear.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/Mecanum_BasicOpMode_Linear.java
@@ -130,10 +130,10 @@ public class Mecanum_BasicOpMode_Linear extends LinearOpMode {
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y - x + rx) / denominator;
-            double backLeftPower = (y + x + rx) / denominator;
-            double frontRightPower = (y + x - rx) / denominator;
-            double backRightPower = (y - x - rx) / denominator;
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
             frontLeft.setPower(frontLeftPower);
             backLeft.setPower(backLeftPower);
