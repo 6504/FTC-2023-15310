1cc375fdd113ac9a2bcedbaed54f2d3b7fe3497b
pradeepravi <pradeepravi2006@gmail.com>
2022-12-06 15:51:53 -0500

    changed package and file location for auto

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/RightSideAuto.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/RightSideAuto.java
@@ -33,6 +33,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
