53a9c768db7205b9b09fff418d5dadaad2ee7598
Ethan N <77598169+EthanNguyen06@users.noreply.github.com>
2022-12-09 17:08:02 -0500

    changed wait times

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ComplexRightSideAuto.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ComplexRightSideAuto.java
@@ -147,13 +147,15 @@ public class ComplexRightSideAuto extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
+        sleep(1000);
+
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
         // encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
         // encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         // encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
         claw.setPosition(CLAW_CLOSE_POSITION);
-        sleep(250);
+        sleep(1000);
         lift.setTargetPosition(1000);
         lift.setPower(1);
         lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
