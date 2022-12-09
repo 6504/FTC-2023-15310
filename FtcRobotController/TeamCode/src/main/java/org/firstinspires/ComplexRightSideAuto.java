03c2025a58315c0a7a2b731d42abe38af5d88d42
Ethan N <77598169+EthanNguyen06@users.noreply.github.com>
2022-12-09 17:36:37 -0500

    small changes

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ComplexRightSideAuto.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ComplexRightSideAuto.java
@@ -163,9 +163,9 @@ public class ComplexRightSideAuto extends LinearOpMode {
         encoderDrive(DRIVE_SPEED, -20, 20, 20, -20, 4.0); //strafe left by 20 inches
 
         lift.setTargetPosition(7500);
-        encoderDrive(DRIVE_SPEED, 57, 57, 57, 57, 12.0); //move straight 57 inches
+        encoderDrive(DRIVE_SPEED, 56, 56, 56, 56, 12.0); //move straight 57 inches
 
-        encoderDrive(DRIVE_SPEED, 12, -12, -12, 12, 4.0); //strafe right 14 inches
+        encoderDrive(DRIVE_SPEED, 14, -14, -14, 14, 4.0); //strafe right 14 inches
         
         sleep(1000);
         lift.setTargetPosition(6500);
@@ -179,7 +179,7 @@ public class ComplexRightSideAuto extends LinearOpMode {
 
         encoderDrive(DRIVE_SPEED, 20, 20, -20, -20, 4.0); //rotate right 90 degrees
         sleep(500);
-        encoderDrive(DRIVE_SPEED, 30, 30, 30, 30, 12.0); //move straight 30 inches
+        encoderDrive(DRIVE_SPEED, 40, 40, 40, 40, 12.0); //move straight 30 inches
 
         sleep(500);
         claw.setPosition(CLAW_CLOSE_POSITION);
@@ -187,7 +187,7 @@ public class ComplexRightSideAuto extends LinearOpMode {
         lift.setTargetPosition(LIFT_HIGH);
         sleep(1000);
 
-        encoderDrive(DRIVE_SPEED, -30, -30, -30, -30, 12.0); //move backwards 30 inches
+        encoderDrive(DRIVE_SPEED, -40, -40, -40, -40, 12.0); //move backwards 30 inches
         encoderDrive(DRIVE_SPEED, -20, -20, 20, 20, 4.0); //rotate left 90 degrees
         
         encoderDrive(DRIVE_SPEED, 8, 8, 8, 8, 4.0); //move straight 8 inches
